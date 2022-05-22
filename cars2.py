6fdf42c5001d6fa44f51058ef7991bfe9b2d4858
Pradhyum23 <76551888+Pradhyum23@users.noreply.github.com>
2022-05-22 15:43:58 +0530

    Add files via upload

---
+++ cars2.py
@@ -0,0 +1,26 @@
+import dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'sl.BIGeanVKYrWaOo2anZoVQ2TXvuHCOxhlzhPg2_mQCCLPe0jwJ2MvNWGeMXK45A_FGIpkwOVYjgaGH-6kooAbrDeu4wRbCYTTLsGlBA-UfQA-JZZLe8KMuJfmBFB7PPDn3fFJJTI'
+    transferData = TransferData(access_token)
+
+    file_from = 'cars.py'
+    file_to = '/project98.txt'  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+if __name__ == '__main__':
+    main() 
